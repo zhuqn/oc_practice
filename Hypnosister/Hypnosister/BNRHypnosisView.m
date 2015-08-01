67c0e98f1940b318b9775c44db9e1df5bd7d990c
zhuqn <zhuqn0822@126.com>
2015-08-01 12:09:51 +0800

    fix funtion

--- Hypnosister/Hypnosister/BNRHypnosisView.m
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -12,35 +12,28 @@
 - (void)drawRect:(CGRect)rect
 {
     CGRect bounds = self.bounds;
-    
     CGPoint center;
     center.x=bounds.origin.x+bounds.size.width/2.0;
     center.y=bounds.origin.y+bounds.size.height/2.0;
-    
-//    float radius = (MIN(bounds.size.width, bounds.size.height)/2.0);
-    
+
     float maxRadius=hypotf(bounds.size.width, bounds.size.height)/2.0;
-    
     UIBezierPath *path=[[UIBezierPath alloc] init];
-//    [path addArcWithCenter:center
-//                    radius:radius startAngle:0
-//                  endAngle:M_PI*2
-//                 clockwise:YES];
-    
+
     for (float currentRadius=maxRadius; currentRadius>0; currentRadius-=20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];//移动笔的位置
         [path addArcWithCenter:center
                         radius:currentRadius
                     startAngle:0.0
                       endAngle:M_PI*2.0
                      clockwise:YES];
     }
-    
-    
     path.lineWidth=10;
-    
     [[UIColor lightGrayColor] setStroke];
-    
     [path stroke];
+    
+    UIImage *logoImage = [UIImage imageNamed:@"logo.png"];
+    CGRect paintImage = CGRectMake(bounds.origin.x+50, bounds.origin.y+100, bounds.size.width-100, bounds.size.height-200);
+    [logoImage drawInRect:paintImage];
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
