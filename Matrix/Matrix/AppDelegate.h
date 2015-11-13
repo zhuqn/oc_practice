ba15edc3911ae397a960ccdcce5b0e6fdb1512c6
zhuqn <zhuqn0822@126.com>
2015-11-13 09:25:09 +0800

    add matrix demo

---
+++ Matrix/Matrix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matrix
+//
+//  Created by zqn on 15/11/13.
+//  Copyright © 2015年 my.company. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Matrix/Matrix/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Matrix
+//
+//  Created by zqn on 15/11/13.
+//  Copyright © 2015年 my.company. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
