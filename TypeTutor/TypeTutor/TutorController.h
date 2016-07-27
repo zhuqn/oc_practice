12916ced345ec40e056e62de14e2bd39cb57b4cb
zhuqn <zhuqn0822@126.com>
2016-07-28 01:11:45 +0800

    Add timer function

---
+++ TypeTutor/TypeTutor/TutorController.h
@@ -0,0 +1,13 @@
+//
+//  TutorController.h
+//  TypeTutor
+//
+//  Created by zqn on 2016/7/28.
+//  Copyright © 2016年 my.company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TutorController : NSObject
+
+@end
---
+++ TypeTutor/TypeTutor/TutorController.m
@@ -0,0 +1,13 @@
+//
+//  TutorController.m
+//  TypeTutor
+//
+//  Created by zqn on 2016/7/28.
+//  Copyright © 2016年 my.company. All rights reserved.
+//
+
+#import "TutorController.h"
+
+@implementation TutorController
+
+@end
